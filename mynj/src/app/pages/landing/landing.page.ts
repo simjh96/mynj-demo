52c89260a20f5a1f0306b9d5850df7ff9abe0691
simjh96 <simjh96@yonsei.ac.kr>
2022-08-01 16:03:02 +0900

    done: android build next: 3js android fix

--- mynj/src/app/pages/landing/landing.page.ts
+++ mynj/src/app/pages/landing/landing.page.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPage {
   isShow = true;
   constructor() {
-    setTimeout(() => {
-      this.isShow = false;
-    }, 4000);
+    // setTimeout(() => {
+    //   this.isShow = false;
+    // }, 10000);
   }
 }
